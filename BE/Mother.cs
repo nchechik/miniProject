85f585e58c84f6ba5f4a4409786582bdffa08786
nchechik <nich26148@gmail.com>
2017-12-06 13:36:33 +0200

    iny f

---
+++ BE/Mother.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class Mother
+    {
+        int x = 0;
+        int f;
+       
+    }
+}
---
+++ BE/enum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    enum day { sunday, monday, Tuesday, Wednesday, Thursday, Friday, Saturday}
+    
+}
